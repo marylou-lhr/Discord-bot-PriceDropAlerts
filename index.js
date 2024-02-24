0e805f09562d15ed4a04776965dd8ef252fcddda
Marylou Lohier <mary.lohier@gmail.com>
2024-02-24 14:23:42 +0100

    Create a try and catch to manage the error according to the discord.js documentation

--- index.js
+++ index.js
@@ -101,12 +101,32 @@ client.on('ready', async () => {
   list_products_prices = '```';
 
   //Making sure that the user did input a slash command
-  client.on(Events.InteractionCreate, interaction => {
+  client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
     console.log(interaction);
 
     
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+  //Trying the command to see if it works when an user wants to execute it
+	try {
+		await command.execute(interaction);
+	} 
+  catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
   });
+  
   //Adding the command
   await client.applicationCommands.set([cmdListPP]);
 
