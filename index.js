c5b74cc1e85ea3dc8e7354ee48540f3a4177a6f9
Marylou Lohier <mary.lohier@gmail.com>
2024-02-24 15:13:46 +0100

    Corrected the errors i didn't notice

--- index.js
+++ index.js
@@ -52,43 +52,43 @@ client.on('ready', async () => {
   // Amazon URL of the products I want the bot to keep an eye on
   //Note to self : Delete this line when I will know that it works
   module.exports = {
-    const products = {
-      'graphicCard': 'B0C49S5R55',
-      'processor': 'B08166SLDF',
-      'motherboard': 'B089HDHVV6',
-      'ram': 'B0B5CDVS7S',
-      'ssd': 'B0BS1WWDQB',
-      'powerSupply': 'B0991TZ399',
-      'case': 'B07YQ724L8',
-      'ventilation': 'B0B5L3K293',
-      'keyboard': 'B07W6JFM87'
-    };
+    data: products = {
+        'graphicCard': 'B0C49S5R55',
+        'processor': 'B08166SLDF',
+        'motherboard': 'B089HDHVV6',
+        'ram': 'B0B5CDVS7S',
+        'ssd': 'B0BS1WWDQB',
+        'powerSupply': 'B0991TZ399',
+        'case': 'B07YQ724L8',
+        'ventilation': 'B0B5L3K293',
+        'keyboard': 'B07W6JFM87'
+        },
   
-    //Images of the products to show in the embedded message
-    const imgProducts = {
-      'graphicCard': 'https://m.media-amazon.com/images/I/714nXgTiuAL._AC_SX425_.jpg',
-      'processor': 'https://m.media-amazon.com/images/I/61twhaihHtL._AC_SL1500_.jpg',
-      'motherboard': 'https://m.media-amazon.com/images/I/51zDXiQ0PqL._AC_SL1500_.jpg',
-      'ram': 'https://m.media-amazon.com/images/I/61YG-Rm-ZAL._AC_SL1500_.jpg',
-      'ssd': 'https://m.media-amazon.com/images/I/51oiVSSsxBL._AC_SL1000_.jpg',
-      'powerSupply': 'https://m.media-amazon.com/images/I/81eLlIQ5PsL._AC_SL1500_.jpg',
-      'case': 'https://m.media-amazon.com/images/I/51NgRrE-OzL._AC_SL1024_.jpg',
-      'ventilation': 'https://m.media-amazon.com/images/I/71MQHn9FlUL._SL1500_.jpg',
-      'keyboard': 'https://m.media-amazon.com/images/I/71x6vEFr69L._AC_SL1500_.jpg'
-    }
-  
-    //Initial prices of each product in order to make comparisons
-    const initialPrices = {
-      'graphicCard': 299.80,
-      'processeur': 163,
-      'carteMere': 113.35,
-      'ram': 63.78,
-      'ssd': 82.73,
-      'powerSupply': 65.99,
-      'case': 64.99,
-      'ventilo': 22.90,
-      'keyboard': 119
-    };
+        //Images of the products to show in the embedded message
+    data: imgProducts = {
+          'graphicCard': 'https://m.media-amazon.com/images/I/714nXgTiuAL._AC_SX425_.jpg',
+          'processor': 'https://m.media-amazon.com/images/I/61twhaihHtL._AC_SL1500_.jpg',
+          'motherboard': 'https://m.media-amazon.com/images/I/51zDXiQ0PqL._AC_SL1500_.jpg',
+          'ram': 'https://m.media-amazon.com/images/I/61YG-Rm-ZAL._AC_SL1500_.jpg',
+          'ssd': 'https://m.media-amazon.com/images/I/51oiVSSsxBL._AC_SL1000_.jpg',
+          'powerSupply': 'https://m.media-amazon.com/images/I/81eLlIQ5PsL._AC_SL1500_.jpg',
+          'case': 'https://m.media-amazon.com/images/I/51NgRrE-OzL._AC_SL1024_.jpg',
+          'ventilation': 'https://m.media-amazon.com/images/I/71MQHn9FlUL._SL1500_.jpg',
+          'keyboard': 'https://m.media-amazon.com/images/I/71x6vEFr69L._AC_SL1500_.jpg'
+        },
+      
+        //Initial prices of each product in order to make comparisons
+    data: initialPrices = {
+          'graphicCard': 299.80,
+          'processeur': 163,
+          'carteMere': 113.35,
+          'ram': 63.78,
+          'ssd': 82.73,
+          'powerSupply': 65.99,
+          'case': 64.99,
+          'ventilo': 22.90,
+          'keyboard': 119
+      }
   }
 
   //Making sure that the user did input a slash command
